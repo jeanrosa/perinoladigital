22f80877568824a30a45f53a14402213022e960f
vento <capoeiravenezuela@gmail.com>
2020-08-11 12:37:24 -0400

    Cambio ppa

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+	<?php include('config/head.php'); ?>
+	<body>
+		<div id="lateral" class="col-12">
+<!--Cabezera Menu-->				
+			<?php include('config/cabezera.php'); ?>
+<!--Cuerpo de Pagina -->				
+			<div id="cuerpo" class="col-9">
+				<section id="quienes" class="filas col-3 redondas amarilla sombras tfondoQuienes ImgComp">
+					<article>
+						<header class="header">
+							<a href="quienes_somos.php">
+							<p class="izquierda tituloHome">¿QUIÉNES</p>
+							<p class="derecha afondoamarillo tituloHome">SOMOS?</p>	
+							</a>
+						</header>
+					</article>
+				</section>
+				<section id="ofrecemos" class="filas col-3 redondas naranja sombras tfondoOfrecemos ImgComp">
+					<article>
+						<header class="header">
+							<a href="quienes_somos.php">
+							<p class="izquierda tituloHome">¿QUÉ TE</p>
+							<p class="derecha afondonaranja tituloHome">OFRECEMOS?</p>	
+							</a>
+						</header>
+					</article>
+				</section>
+				<section id="donde" class="filas col-3 redondas verde sombras tfondoDonde ImgComp">
+					<article>
+						<header class="header">
+							<a href="quienes_somos.php">
+							<p class="izquierda tituloHome">¿DÓNDE</p>
+							<p class="derecha afondoverde tituloHome">ESTÁMOS?</p>	
+							</a>
+						</header>
+					</article>
+				</section>
+				<section id="contacto" class="filas col-3 redondas azul sombras tfondoContacto ImgComp">
+					<article>
+						<header class="header">
+							<a href="contacto.php">
+							<p class="izquierda tituloHome">-</p>
+							<p class="derecha afondoazul tituloHome">CONTÁCTANOS</p>	
+							</a>
+						</header>	
+					</article>
+				</section>
+			</div>
+			<!--Pie de Pagina -->
+			<?php include('config/pie.php'); ?>
+		</div>
+	</body>
+</html>
